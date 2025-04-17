d838a1cd49f1ea59478cc5a5095f00bfe90a1361
Derin DeNio <derindenio@gmail.com>
2025-04-16 18:31:10 -0600

    Practiced JS, given an array of objects representing data about developers who have signed up to attend the next coding meet up, return an array whewre each object will have all the previous properties AND  a new property called greeting that has their first name and asks what they like most about the language in their object

--- randommmm/js/main.js
+++ randommmm/js/main.js
@@ -1,7 +1,7 @@
 function sortByValueAndIndex(arr){
-  //determine which items in array have no duplicates (only appear once)
-  //filter method
-  return arr.filter(e => arr.indexOf(e) === arr.lastIndexOf(e))
+  //return an array where each object has old properties AND new 'gretting' property with following string value: hi <person's name>, what do you like most about <language here>?
+  //map method to modify each object, use {... } spread operator to keep all old properties, then add new greeting property
+  return arr.map(e => ({...e, greeting: `Hi ${e.firstName}, what do you like most about ${e.language}?`}))
 }
 
 //console.log(sortByValueAndIndex(["3:1","2:2","0:1","4:0"]))
@@ -15,7 +15,7 @@ function sortByValueAndIndex(arr){
 //console.log(sortByValueAndIndex([22,-6,32,82,9,25])) 
 //console.log(sortByValueAndIndex("abcdeeeeewwwwwwllllppp", "xxxxzazzzzfff"))
 //console.log(sortByValueAndIndex("1 -5 2 10 15 3")) 
-console.log(sortByValueAndIndex([1,2,1,2,3,3,4,5,6,6]))
+//console.log(sortByValueAndIndex([1,2,1,2,3,3,4,5,6,6]))
 //console.log(sortByValueAndIndex(["1",2,"5",10,"23"]))
 //console.log(sortByValueAndIndex(['true','flick','taco','flick','flick','green']))
 //console.log(sortByValueAndIndex(''))
@@ -30,7 +30,12 @@ console.log(sortByValueAndIndex([1,2,1,2,3,3,4,5,6,6]))
 //console.log(sortByValueAndIndex("ortsp", ["sport", "parrot", "ports", "matey"]))
 //console.log(sortByValueAndIndex([["Chad",2],["Tommy",9],["Clara",4],["Tamara",5],["colton",10]], 7))
 //console.log(sortByValueAndIndex({"java":10, "Ruby":80,"Python":65}))
-//console.log(sortByValueAndIndex([1,2,11,4,5], lessThan9))
+//console.log(sortByValueAndIndex([1,2,11,4,5], lessThan9)) 
+console.log(sortByValueAndIndex([
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+]))
 // function sortByValueAndIndex(jewels,stones) {
 //   let final = []
 //    const map = {}
