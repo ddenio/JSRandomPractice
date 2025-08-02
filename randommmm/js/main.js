e8f979ec476fcd2e45ffb2fad2dca684411dee6b
Derin DeNio <derindenio@gmail.com>
2025-08-01 19:20:04 -0600

    Practiced problem that when given an array of string characters and an array of integers to return just the characters from arr1 that have the index of each arr2 value

--- randommmm/js/main.js
+++ randommmm/js/main.js
@@ -1,7 +1,7 @@
-function sortByValueAndIndex(arr1){
-  //how do we sort an array of numbers from lowest to hights?
-  //sort function, but with (a,b) => a-b
-  return arr1.sort((a,b) => a-b)
+function sortByValueAndIndex(arr1,arr2){
+  //You are given two arrays arr1 and arr2, where arr2 always contains integers. Arr2 is the indexes of arr1 that we want to keep the values of.
+  //thinking map function to map arr2 with the values from arr1
+  return arr2.map(e => arr1[e])
 }
 
 //console.log(sortByValueAndIndex(["3:1","2:2","0:1","4:0"]))
@@ -12,7 +12,7 @@ function sortByValueAndIndex(arr1){
 //console.log(sortByValueAndIndex("1 7 3 4"))
 //console.log(sortByValueAndIndex(69))
 //console.log(sortByValueAndIndex([1,2,3,4,5],[1,3,5,7])) 
-console.log(sortByValueAndIndex([22,-6,32,82,9,25])) 
+//console.log(sortByValueAndIndex([22,-6,32,82,9,25])) 
 //console.log(sortByValueAndIndex("abcdeeeeewwwwwwllllppp", "xxxxzazzzzfff"))
 //console.log(sortByValueAndIndex("1 -5 2 10 15 3")) 
 //console.log(sortByValueAndIndex([1,2,1,2,3,3,4,5,6,6]))
@@ -34,6 +34,7 @@ console.log(sortByValueAndIndex([22,-6,32,82,9,25]))
 //console.log(sortByValueAndIndex([17,17,17,3,17,17,17]))
 //console.log(sortByValueAndIndex(["a","b","c","d"], [3,0,2,8,3])) 
 //console.log(sortByValueAndIndex("Beggegeggineggnegg"))
+console.log(sortByValueAndIndex(['a','b','c','d'], [2,0,2,3,0]))
 // console.log(sortByValueAndIndex([
 //   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
 //   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
