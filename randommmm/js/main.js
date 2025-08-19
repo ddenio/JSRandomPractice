86646868f7f497824fba52907f61030d9e0678dc
Derin DeNio <derindenio@gmail.com>
2025-08-18 18:51:07 -0600

    practiced js problem that when given an object of key-value pairs to return just the names of hte languages that are above a certain value, in ascending order

--- randommmm/js/main.js
+++ randommmm/js/main.js
@@ -1,7 +1,7 @@
-    //How could we Define a method/function that removes from a given array of integers all the values contained in a second array.
-    //use filter method, use include method
-    function sortByValueAndIndex(arr1, arr2){
-       return arr1.filter(e => !arr2.includes(e))
+    //How could I given an object of key-value pairs, like : {"Java" : 10, "Ruby" : 80, "Python" : 65}, or {"Hindi" : 60, "Greek" : 71, "Dutch" : 93}, return just the names of the languages that had a score above 59, and are sorted by descending score?
+    function sortByValueAndIndex(obj){
+        //filter method? object.keys
+       return Object.keys(obj).filter(e => obj[e] > 59).sort((a,b) => obj[b] - obj[a])
     }
 
 //console.log(sortByValueAndIndex(["3:1","2:2","0:1","4:0"]))
@@ -11,7 +11,7 @@
 //console.log(sortByValueAndIndex("taco sauce green cat no tacocart")) 
 //console.log(sortByValueAndIndex("1 7 3 4"))
 //console.log(sortByValueAndIndex(72971))
-console.log(sortByValueAndIndex([1,2,3,4,5],[1,3,5,7])) 
+//console.log(sortByValueAndIndex([1,2,3,4,5],[1,3,5,7])) 
 //console.log(sortByValueAndIndex([22,-6,32,82,9,25])) 
 //console.log(sortByValueAndIndex("abcdeeeeewwwwwwllllppp", "xxxxzazzzzfff"))
 //console.log(sortByValueAndIndex("1 -5 2 10 15 3")) 
@@ -29,7 +29,7 @@ console.log(sortByValueAndIndex([1,2,3,4,5],[1,3,5,7]))
 //console.log(sortByValueAndIndex("ainstuomn", ["mountains", "hills", "mesa"]))
 //console.log(sortByValueAndIndex("ortsp", ["sport", "parrot", "ports", "matey"]))
 //console.log(sortByValueAndIndex([["Chad",2],["Tommy",9],["Clara",4],["Tamara",5],["colton",10]], 7))
-//console.log(sortByValueAndIndex({"java":10, "Ruby":80,"Python":65, "RubyonRails":92}))
+console.log(sortByValueAndIndex({"java":10, "Ruby":80,"Python":65, "RubyonRails":92}))
 //console.log(sortByValueAndIndex([1,2,11,4,5], lessThan9)) 
 //console.log(sortByValueAndIndex([17,17,17,"a",17,17,17]))
 //console.log(sortByValueAndIndex(["a","b","c","d"], [3,0,2,8,3])) 
