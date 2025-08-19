f8c6efccd87bc2fe31e612ee50befb67b2b154b9
Derin DeNio <derindenio@gmail.com>
2025-08-18 18:57:10 -0600

    Practiced js problem that if given a name to return 'hello, name' and if nothing is available or given a number to return 'hello world'

--- randommmm/js/main.js
+++ randommmm/js/main.js
@@ -1,7 +1,15 @@
-    //How could I given an object of key-value pairs, like : {"Java" : 10, "Ruby" : 80, "Python" : 65}, or {"Hindi" : 60, "Greek" : 71, "Dutch" : 93}, return just the names of the languages that had a score above 59, and are sorted by descending score?
+//     How do we solve this JavaScript problem:
+
+// Define a method hello that returns "Hello, Name!" to a given name, or says Hello, World! if name is not given (or passed as an empty String).
+// Assuming that name is a String and it checks for user typos to return a name with a first capital letter (Xxxx).
+// Examples:
+// * With `name` = "john"  => return "Hello, John!"
+// * With `name` = "aliCE" => return "Hello, Alice!"
+// * With `name` not given 
+//   or `name` = ""        => return "Hello, World!"
+//typeof = 'string' to find if its a name, then use string[0].toUpperCase() + string.slice(1).toLowerCase() to get the case right
     function sortByValueAndIndex(obj){
-        //filter method? object.keys
-       return Object.keys(obj).filter(e => obj[e] > 59).sort((a,b) => obj[b] - obj[a])
+       return typeof obj === 'string' ? `Hello, ${obj[0].toUpperCase()+obj.slice(1).toLowerCase()}!` : "Hello, World!"
     }
 
 //console.log(sortByValueAndIndex(["3:1","2:2","0:1","4:0"]))
@@ -19,7 +27,7 @@
 //console.log(sortByValueAndIndex(["1",2,"5",10,"23"]))
 //console.log(sortByValueAndIndex(['true','flick','taco','flick','flick','green']))
 //console.log(sortByValueAndIndex(''))
-//console.log(sortByValueAndIndex('jOhnAthAn'))
+console.log(sortByValueAndIndex(324354))
 //console.log(sortByValueAndIndex('aA', 'aAaBBBBBB'))
 //console.log(sortByValueAndIndex("the green blob ran ban super fast"))
 //console.log(sortByValueAndIndex([1,2,3,4,5]))
@@ -29,7 +37,7 @@
 //console.log(sortByValueAndIndex("ainstuomn", ["mountains", "hills", "mesa"]))
 //console.log(sortByValueAndIndex("ortsp", ["sport", "parrot", "ports", "matey"]))
 //console.log(sortByValueAndIndex([["Chad",2],["Tommy",9],["Clara",4],["Tamara",5],["colton",10]], 7))
-console.log(sortByValueAndIndex({"java":10, "Ruby":80,"Python":65, "RubyonRails":92}))
+//console.log(sortByValueAndIndex({"java":10, "Ruby":80,"Python":65, "RubyonRails":92}))
 //console.log(sortByValueAndIndex([1,2,11,4,5], lessThan9)) 
 //console.log(sortByValueAndIndex([17,17,17,"a",17,17,17]))
 //console.log(sortByValueAndIndex(["a","b","c","d"], [3,0,2,8,3])) 
