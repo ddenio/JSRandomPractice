853b66baef7f161ad1761ea38f7baf76428d4feb
Derin DeNio <derindenio@gmail.com>
2025-08-21 17:32:43 -0600

    Practiced js problem that when given an array of objects, return all current properties, and then add a new property called 'greeting' that greets the user with their first name and language

--- randommmm/js/main.js
+++ randommmm/js/main.js
@@ -1,12 +1,29 @@
-// What if we need the length of the words separated by a space to be added at the end of that same word and have it returned as an array?
-// Example(Input --> Output)
-// "apple ban" --> ["apple 5", "ban 3"]
-// "you will win" -->["you 3", "will 4", "win 3"]
-// Your task is to write a function that takes a String and returns an Array/list with the length of each word added to each element .
-// Note: String will have at least one element; words will always be separated by a space.
-    //going to split string by space, then map method, using word.length
+// How could i solve this JS problem:
+
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+// Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+// Hi < firstName here >, what do you like the most about < language here >?
+// For example, given the following input array:
+// var list1 = [
+//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+// ];
+// your function should return the following array:
+// [
+//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+//     greeting: 'Hi Sofia, what do you like the most about Java?'
+//   },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+//     greeting: 'Hi Lukas, what do you like the most about Python?'
+//   },
+//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+//     greeting: 'Hi Madison, what do you like the most about Ruby?'
+//   } 
+// ];
+        //use map method and spread operator to get all current properties of objects, and then map new 'greeting' property
     function sortByValueAndIndex(arr){
-       return arr.split(' ').map(e => `${e} ${e.length}`)
+       return arr.map(e => ({...e, greeting: `Hi ${e.firstName}, what do you like most about ${e.language}?`}))
     }
 
 //console.log(sortByValueAndIndex())
@@ -14,7 +31,7 @@
 //console.log(sortByValueAndIndex("a         **&*&c"))
 //console.log(sortByValueAndIndex(['a','b','b','d']))
 // console.log(sortByValueAndIndex("September 5, 2014", "January 7, 2014"))
-console.log(sortByValueAndIndex("taco sauce green cat no tacocart")) 
+//console.log(sortByValueAndIndex("taco sauce green cat no tacocart")) 
 //console.log(sortByValueAndIndex("1 7 3 4"))
 //console.log(sortByValueAndIndex(72971))
 //console.log(sortByValueAndIndex([1,2,3,4,5],[1,3,5,7])) 
@@ -43,11 +60,11 @@ console.log(sortByValueAndIndex("taco sauce green cat no tacocart"))
 //console.log(sortByValueAndIndex(['a','b','c','d'], [2,0,2,3,0,5]))
 //console.log(sortByValueAndIndex([1,3,4,5,6,7], [1,2,3,4,5,6,7,8]))
 //console.log(sortByValueAndIndex("codewars"))
-// console.log(sortByValueAndIndex([
-//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
-//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
-// ]))
+console.log(sortByValueAndIndex([
+  { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+  { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+  { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' } 
+]))
 // function sortByValueAndIndex(jewels,stones) {
 //   let final = []
 //    const map = {}
