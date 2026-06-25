968583a18ce89d071c700bf6c29c152c30a7f4cc
Derin DeNio <derindenio@gmail.com>
2026-06-25 17:29:29 -0600

    Practiced JS problem to return an array of objects, but add a new property into each object, a greeting property

--- randommmm/js/main.js
+++ randommmm/js/main.js
@@ -1,12 +1,34 @@
-// How can I write a function to convert a list of string numbers, seperated by a colon :, into a list of comma seperated numbers?
+// How could i solve this JS problem:
 
-// e.g. starting out with let matches = ["3:1", "2:2", "0:1", "4:0", "1:2", "2:0", "3:3", "1:0", "0:0", "2:1"];
+// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
+// Your task is to return an array where each object will have a new property 'greeting' with the following string value:
+// Hi < firstName here >, what do you like the most about < language here >?
+// For example, given the following input array:
+// var list1 = [
+//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
+//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
+// ];
+// your function should return the following array:
+// [
+//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
+//     greeting: 'Hi Sofia, what do you like the most about Java?'
+//   },
+//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
+//     greeting: 'Hi Lukas, what do you like the most about Python?'
+//   },
+//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
+//     greeting: 'Hi Madison, what do you like the most about Ruby?'
+//   }
+// ];
+
+//What I would do: I would map the arr, and add in the greeting property that way, i can use spread operator to include the existing properties
 
 function sortByValueAndIndex(arr) {
-  return arr.map((e) => [e.split(":").join(",")]);
+  return arr.map((e) => ({ ...e, greeting: `Hi ${e.firstName}, what do you like the most about ${e.language}?` }));
 }
 
-console.log(sortByValueAndIndex(["3:1", "2:2", "0:1", "4:0"]));
+//console.log(sortByValueAndIndex(["3:1", "2:2", "0:1", "4:0"]));
 // console.log(
 //   sortByValueAndIndex("ortsp", ["sport", "parrot", "ports", "matey"]),
 // );
@@ -43,11 +65,34 @@ console.log(sortByValueAndIndex(["3:1", "2:2", "0:1", "4:0"]));
 //console.log(sortByValueAndIndex([1,3,4,5,6,7], [1,2,3,4,5,6,7,8]))
 //console.log(sortByValueAndIndex("codewars"))
 //console.log(sortByValueAndIndex("xyaabbbccccdefww", "xxxxyyyyabklmopq"));
-// console.log(sortByValueAndIndex([
-//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
-//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
-// ]))
+console.log(
+  sortByValueAndIndex([
+    {
+      firstName: "Sofia",
+      lastName: "I.",
+      country: "Argentina",
+      continent: "Americas",
+      age: 35,
+      language: "Java",
+    },
+    {
+      firstName: "Lukas",
+      lastName: "X.",
+      country: "Croatia",
+      continent: "Europe",
+      age: 35,
+      language: "Python",
+    },
+    {
+      firstName: "Madison",
+      lastName: "U.",
+      country: "United States",
+      continent: "Americas",
+      age: 32,
+      language: "Ruby",
+    },
+  ]),
+);
 // function sortByValueAndIndex(jewels,stones) {
 //   let final = []
 //    const map = {}
