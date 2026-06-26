24f4d11b79442f952cfb70046893f1e1ffc49d67
Derin DeNio <derindenio@gmail.com>
2026-06-26 17:36:33 -0600

    Practiced JS problem to iteratre though each digit of a number and sum the digits together

--- randommmm/js/main.js
+++ randommmm/js/main.js
@@ -1,31 +1,11 @@
-// How could i solve this JS problem:
+// How can we iterate through each digit in a Number and sum the digits together?
+//Convert our number to a string, use the split method to pput the digits into an array, then we can use the reduce method to Sum the digits, with
+//    converting each digit back into a number
 
-// You will be given an array of objects (associative arrays in PHP, tables in COBOL) representing data about developers who have signed up to attend the next coding meetup that you are organising.
-// Your task is to return an array where each object will have a new property 'greeting' with the following string value:
-// Hi < firstName here >, what do you like the most about < language here >?
-// For example, given the following input array:
-// var list1 = [
-//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java' },
-//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python' },
-//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby' }
-// ];
-// your function should return the following array:
-// [
-//   { firstName: 'Sofia', lastName: 'I.', country: 'Argentina', continent: 'Americas', age: 35, language: 'Java',
-//     greeting: 'Hi Sofia, what do you like the most about Java?'
-//   },
-//   { firstName: 'Lukas', lastName: 'X.', country: 'Croatia', continent: 'Europe', age: 35, language: 'Python',
-//     greeting: 'Hi Lukas, what do you like the most about Python?'
-//   },
-//   { firstName: 'Madison', lastName: 'U.', country: 'United States', continent: 'Americas', age: 32, language: 'Ruby',
-//     greeting: 'Hi Madison, what do you like the most about Ruby?'
-//   }
-// ];
-
-//What I would do: I would map the arr, and add in the greeting property that way, i can use spread operator to include the existing properties
-
-function sortByValueAndIndex(arr) {
-  return arr.map((e) => ({ ...e, greeting: `Hi ${e.firstName}, what do you like the most about ${e.language}?` }));
+function sortByValueAndIndex(num) {
+  return String(num)
+    .split("")
+    .reduce((sum, digit) => sum + Number(digit), 0);
 }
 
 //console.log(sortByValueAndIndex(["3:1", "2:2", "0:1", "4:0"]));
@@ -37,7 +17,7 @@ function sortByValueAndIndex(arr) {
 // console.log(sortByValueAndIndex("September 5, 2014", "January 7, 2014"))
 //console.log(sortByValueAndIndex("taco sauce green cat no tacocart"))
 //console.log(sortByValueAndIndex("1 7 3 4"))
-//console.log(sortByValueAndIndex(72971))
+console.log(sortByValueAndIndex(72971));
 //console.log(sortByValueAndIndex([1,2,3,4,5],[1,3,5,7]))
 //console.log(sortByValueAndIndex([22, -6, 32, 82, 9, 25]));
 //console.log(sortByValueAndIndex("abcdeeeeewwwwwwllllppp", "xxxxzazzzzfff"));
@@ -65,34 +45,34 @@ function sortByValueAndIndex(arr) {
 //console.log(sortByValueAndIndex([1,3,4,5,6,7], [1,2,3,4,5,6,7,8]))
 //console.log(sortByValueAndIndex("codewars"))
 //console.log(sortByValueAndIndex("xyaabbbccccdefww", "xxxxyyyyabklmopq"));
-console.log(
-  sortByValueAndIndex([
-    {
-      firstName: "Sofia",
-      lastName: "I.",
-      country: "Argentina",
-      continent: "Americas",
-      age: 35,
-      language: "Java",
-    },
-    {
-      firstName: "Lukas",
-      lastName: "X.",
-      country: "Croatia",
-      continent: "Europe",
-      age: 35,
-      language: "Python",
-    },
-    {
-      firstName: "Madison",
-      lastName: "U.",
-      country: "United States",
-      continent: "Americas",
-      age: 32,
-      language: "Ruby",
-    },
-  ]),
-);
+// console.log(
+//   sortByValueAndIndex([
+//     {
+//       firstName: "Sofia",
+//       lastName: "I.",
+//       country: "Argentina",
+//       continent: "Americas",
+//       age: 35,
+//       language: "Java",
+//     },
+//     {
+//       firstName: "Lukas",
+//       lastName: "X.",
+//       country: "Croatia",
+//       continent: "Europe",
+//       age: 35,
+//       language: "Python",
+//     },
+//     {
+//       firstName: "Madison",
+//       lastName: "U.",
+//       country: "United States",
+//       continent: "Americas",
+//       age: 32,
+//       language: "Ruby",
+//     },
+//   ]),
+// );
 // function sortByValueAndIndex(jewels,stones) {
 //   let final = []
 //    const map = {}
